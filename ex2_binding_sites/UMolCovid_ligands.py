0bfd4a7bf91479d465ca76987edaf1db4979168b
Marc Baaden <baaden@smplinux.de>
2020-08-29 17:52:53 +0200

    script and pdb innitial verions

---
+++ ex2_binding_sites/UMolCovid_ligands.py
@@ -0,0 +1,64 @@
+#Set screen resolution, mandatory for recording in raytracing mode
+Screen.SetResolution(1920, 1080, False)
+
+#Useful when recording a long video
+Application.runInBackground = True
+
+bg_color("gray")
+#Avoid pre-computing surfaces for memory and perf purpose
+UnityMolMain.disableSurfaceThread = True
+
+#Start raytracing mdoe
+UnityMolMain.raytracingMode = True
+
+#Not used in raytracing mode
+enableDepthCueing()
+setDepthCueingStart(-2.92)
+setDepthCueingDensity(0.50)
+
+enableDOF()
+setDOFFocusDistance(0.448)
+setDOFFocalLength(27.652)
+setDOFAperture(7.520)
+
+
+def renderVideo(path):
+    pdbids = ["5RGG","5RGI","5RGH","5RG3","5RG2","5RG1","5RGS","5RGR","5RGK","5RGJ","5RGM","5RGL","5RGO","5RGN","5RGQ","5RGP","5R8T","5REO","5REN","5RFZ","5RFY","5RFR","5RFQ","5RFT","5RFS","5RFV","5RFU","5RFX","5RFW","5RFJ","5RFI","5RFL","5RFK","5RFN","5RFM","5RFP","5RFO","5RG0","5REA","5REC","5REB","5REE","5RED","5REG","5REF","5RE9","5RE8","5RE5","5RE4","5RE7","5RE6","5RFB","5RFA","5RFD","5RFC","5RFF","5RFE","5RFH","5RFG","5REY","5REX","5RF9","5REZ","5RF2","5REP","5RF1","5RES","5RF4","5RER","5RF3","5REU","5RF6","5RET","5RF5","5REW","5RF8","5REV","5RF7","5REI","5REH","5REK","5REJ","5REM","5REL","5RF0","5R84","5R83","5R7Y","5R80","5R82","5R81","5R7Z"]
+    startVideo(path, 1920, 1080, 60, pauseAtStart=True)
+    for i in pdbids[:10]:
+        s = fetch(i, showDefaultRep=False, center=False, bioAssembly=True)
+        setMolParentTransform(Vector3(0.0036, 0.0101, -1.4304), Vector3(0.0168, 0.0168, 0.0168), Vector3(356.6070, 50.6462, 147.5536), Vector3(0.0000, 0.0000, -1.4250), False)
+        sel = select(s.name+" and not protein and not water and not resname DMS PEG IMD", s.name+"_notprotwations", setAsCurrentSelection=False)
+        showSelection(sel.name, "hb")
+        protChain = 0
+        for c in s.currentModel.GetChains():
+            if c.Count > 1000:
+                sel = select(c.ToSelectionMDA(), c.ToSelectionName(), setAsCurrentSelection=False);
+                showSelection(sel.name, "s", True, True, True, SurfMethod.MSMS)
+                if protChain % 2 == 0:
+                    setTransparentSurface(sel.name, 0.20)
+                    showSelection(sel.name, "c")
+                    colorSelection(sel.name, "c", Color(0.97, 0.535, 0.03, 1.0))
+                    setRTMaterialType(sel.name, "s", 5)
+                else:
+                    colorSelection(sel.name, "s", Color(0.4, 0.6, 0.95, 1.0))
+                protChain += 1
+        # setHyperBallMetaphore(sel.name, "vdw", False)
+        annotate2DText(Vector2(0.1,0.1), 3.0, s.name, Color.black)
+        #Restart RT frame !
+        RaytracerManager.Instance.restartRTFrame();
+        yield APIPython.pythonConsole.waitFrames(1)
+        # while RaytracerManager.Instance.denoiserRan() == False and RaytracerManager.Instance.getRTFrameId() < 30:
+        while RaytracerManager.Instance.getRTFrameId() < 15: #Wait for enough RT sampling
+            yield APIPython.pythonConsole.waitFrames(1)
+        unpauseVideo()#Record several frames
+        yield APIPython.pythonConsole.waitFrames(90)#Record 1.5 sec (60 fps = 1 sec)
+        pauseVideo()
+        delete(s.name)
+        clearAnnotations()
+    stopVideo()#Done
+    Application.OpenURL(path)#Open the recorded video
+
+
+APIPython.pythonConsole.doCoroutine(renderVideo("C:/Users/Xavier/Desktop/CovidTestUMol.mp4"))
+
---
+++ ex2_binding_sites/UMol_spike630video.py
@@ -0,0 +1,92 @@
+
+Application.runInBackground = True
+UnityMolMain.disableSurfaceThread = True
+
+bleu1 = ColorUtility.TryParseHtmlString("#6baed6")[1]
+bleu2 = ColorUtility.TryParseHtmlString("#3182bd")[1]
+bleu3 = ColorUtility.TryParseHtmlString("#08519c")[1]
+vert1 = ColorUtility.TryParseHtmlString("#74c476")[1]
+vert2 = ColorUtility.TryParseHtmlString("#31a354")[1]
+vert3 = ColorUtility.TryParseHtmlString("#006d2c")[1]
+oran1 = ColorUtility.TryParseHtmlString("#fd8d3c")[1]
+
+def rep1():
+    print("Representation of the overall ectodomain")
+    #showBoundingBox("6cs2")
+    select("6cs2 and chain A and resid 14:667", "A.S1", True, True, True, True, False, False, True)
+    showSelection("A.S1", "hb")
+    setHyperBallMetaphore("A.S1", "vdw", True)
+    colorSelection("A.S1", "hb", bleu1)
+    setRepSize("A.S1", "hb", 3.01)
+    
+    select("6cs2 and chain C and resid 14:667", "C.S1", True, True, True, True, False, False, True)
+    showSelection("C.S1", "hb")
+    setHyperBallMetaphore("C.S1", "vdw", True)
+    colorSelection("C.S1", "hb", bleu3)
+    setRepSize("C.S1", "hb", 3.01)
+    
+    select("6cs2 and chain A and resid 655:1177", "A.S2", True, True, True, True, False, False, True)
+    showSelection("A.S2", "hb")
+    setHyperBallMetaphore("A.S2", "vdw", True)
+    colorSelection("A.S2", "hb", vert1)
+    setRepSize("A.S2", "hb", 3.01)
+    
+    select("6cs2 and chain B and resid 655:1177", "B.S2", True, True, True, True, False, False, True)
+    showSelection("B.S2", "hb")
+    setHyperBallMetaphore("B.S2", "vdw", True)
+    colorSelection("B.S2", "hb", vert2)
+    setRepSize("B.S2", "hb", 3.01)
+    
+    select("6cs2 and chain C and resid 655:1177", "C.S2", True, True, True, True, False, False, True)
+    showSelection("C.S2", "hb")
+    setHyperBallMetaphore("C.S2", "vdw", True)
+    colorSelection("C.S2", "hb", vert3)
+    setRepSize("C.S2", "hb", 3.01)
+    
+    select("6cs2 and chain D", "D.ACE", True, True, True, True, False, False, True)
+    showSelection("D.ACE", "hb")
+    setHyperBallMetaphore("D.ACE", "vdw", True)
+    colorSelection("D.ACE", "hb", oran1)
+    setRepSize("D.ACE", "hb", 3.01)
+    
+    hideSelection("6cs2_not_protein_nucleic", "hb")
+    showSelection("6cs2_not_protein_nucleic", "s")
+    showSelection("6cs2_not_protein_nucleic", "s", SurfMethod.MSMS)
+    colorSelection("6cs2_not_protein_nucleic", "s", RGBA(1.0, 1.0, 0.0, 1.0))
+    
+    select("6cs2 and chain B and resid 14:667", "B.S1", True, True, True, True, False, False, True)
+    showSelection("B.S1", "hb")
+    setHyperBallMetaphore("B.S1", "vdw", True)
+    colorSelection("B.S1", "hb", bleu2)
+    setRepSize("B.S1", "hb", 3.01)
+    
+    clearSelections()
+
+def animation():
+    rep1()
+    setSolidCartoon("6cs2_protein_or_nucleic")
+    setStructurePositionRotation("6cs2", Vector3(0.0000, 0.0000, 0.0000), Vector3(0.0000, 0.0000, 0.0000))
+    setHyperBallMetaphore("vdw")
+    #View 1
+    # setMolParentTransform( Vector3(-1.1592, 1.0424, -0.2460), Vector3(0.0064, 0.0064, 0.0064), Vector3(87.2821, 97.7300, 270.5491), Vector3(0.0000, 0.0000, -1.5500), lerp=False)
+    # yield APIPython.pythonConsole.waitSeconds(1)
+    setMolParentTransform( Vector3(-1.1606, 1.2043, -2.7047), Vector3(0.0064, 0.0064, 0.0064), Vector3(0.0419, 357.5057, 177.4474), Vector3(0.0000, 0.0000, -1.5500), lerp=False)
+    yield APIPython.pythonConsole.waitSeconds(1.0)
+    setMolParentTransform( Vector3(-1.4590, 0.5564, -3.9388), Vector3(0.0064, 0.0064, 0.0064), Vector3(353.2155, 356.5977, 200.2242), Vector3(0.0500, 0.0500, -2.6750), duration=7.5)
+    yield APIPython.pythonConsole.waitSeconds(7.5)
+    setHyperBallMetaphore("smooth", lerp=True, duration=2.0)
+    setMolParentTransform( Vector3(-1.4590, 0.5564, -4.5638), Vector3(0.0064, 0.0064, 0.0064), Vector3(353.2155, 356.5977, 200.2242), Vector3(0.0500, 0.0500, -3.3000), duration=7.5)
+    yield APIPython.pythonConsole.waitSeconds(7.5)
+    yield APIPython.pythonConsole.waitSeconds(2.0)    
+    #Go back now
+    setMolParentTransform( Vector3(-1.4590, 0.5564, -3.9388), Vector3(0.0064, 0.0064, 0.0064), Vector3(353.2155, 356.5977, 200.2242), Vector3(0.0500, 0.0500, -2.6750), duration=7.5)
+    yield APIPython.pythonConsole.waitSeconds(7.5)
+    setHyperBallMetaphore("vdw", lerp=True, duration=5.0)
+    setMolParentTransform( Vector3(-1.1606, 1.2043, -2.7047), Vector3(0.0064, 0.0064, 0.0064), Vector3(0.0419, 357.5057, 177.4474), Vector3(0.0000, 0.0000, -1.5500), duration=5.0)
+
+
+load("fair_covid/ex1_spike/6cs2.pdb")
+
+enableOutline()
+
+APIPython.pythonConsole.doCoroutine(animation())
